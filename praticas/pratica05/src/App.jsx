5d274f4f006921213dd75808162d3323d2a16812
j-b-silva <julia.b.silva@iesb.edu.br>
2024-09-13 21:06:57 -0300

    Iniciando a estilização dos componentes - Pratica 05

--- praticas/pratica05/src/App.jsx
+++ praticas/pratica05/src/App.jsx
@@ -1,10 +1,14 @@
+import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Perfil from "./pages/Perfil";
 
 function App(){
   const index = 1;
   return(
     <>
-      <Login/>
+      {index == 1 && <Login/>}
+      {index == 2 && <Home/>}
+      {index == 3 && <Perfil/>}
     </>
   );
 }
